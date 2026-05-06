874e977ff74cd08e4b5c3d90c454ec06c1072e1c
MatuGon <gongoramatiasivan@gamil.com>
2026-05-05 22:10:05 -0300

    no guarda el darkmode

--- src/components/sidebar.svelte
+++ src/components/sidebar.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { ROUTES } from "@core/utils/router";
-    import{appStore} from "@core/utils/store.svelte";
+    import { appStore } from "@core/utils/store.svelte";
 
     let { activePage } = $props();
 </script>
 
-<aside class=" bg-gray-200 w-60 flex flex-col gap-2 p-2">
+<aside class="bg-gray-200 dark:bg-gray-800 w-60 flex flex-col gap-2 p-2 transition-colors duration-300">
     <div class="p-2">
         <h1 class="text-center font-bold uppercase border-4">Mi Inventario</h1>
     </div>
@@ -18,8 +18,11 @@
     {/each}
 
     <div class="p-2 flex justify-center">
-        <button onclick={() => appStore.toggledarkmode()} class="bg-gray-800 text-white px-4 py-2">
-            {appStore.isdarkmode ? '☀️' : '🌙'}
+        <button
+            onclick={() => appStore.toggledarkmode()}
+            class="bg-gray-800 text-white dark:bg-white dark:text-black px-4 py-2"
+        >
+            {appStore.isdarkmode ? "☀️" : "🌙"}
         </button>
     </div>
 </aside>
--- src/core/utils/store.svelte.ts
+++ src/core/utils/store.svelte.ts
@@ -1,8 +1,8 @@
-class Appstore{
-    isdarkmode= $state(false);
-    issidebaropen= $state(true);
+class Appstore {
+    isdarkmode = $state(false);
+    issidebaropen = $state(true);
 
-    toggledarkmode(){
+    toggledarkmode() {
         this.isdarkmode = !this.isdarkmode;
         localStorage.setItem('color-theme', this.isdarkmode.toString());
         window.document.documentElement.classList.toggle('dark', this.isdarkmode);
